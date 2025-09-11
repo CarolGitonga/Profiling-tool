c5b8a18fd22585b9e6948f2c2e98b809bee63f19
CarolGitonga <carolgitongaofficial@gmail.com>
2025-09-11 17:34:24 +0300

    Create sherlock app

--- people_profiling/urls.py
+++ people_profiling/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
+    path('sherlock/', include('sherlock.urls')),
 ]
 
 # Serve media and static files in development
