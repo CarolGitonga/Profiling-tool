b638e4bba7916ec6acd6ae3607fc4d24c4e93513
CarolGitonga <carolgitongaofficial@gmail.com>
2025-09-15 12:15:53 +0300

    Add HD picture for instagram

--- profiles/utils/instagram_scraper.py
+++ profiles/utils/instagram_scraper.py
@@ -1,19 +1,29 @@
+# raw scraping logic (API calls, parsing, return dicts)
+import logging
 import instaloader
 import os
 from profiles.models import Profile, SocialMediaAccount
 from django.conf import settings
 
 
-def scrape_instagram_profile(username):
+def scrape_instagram_profile(username: str) -> dict | None:
     try:
-        # Step 3: Load the session into Instaloader
+        # Initialize Instaloader
         L = instaloader.Instaloader()
+        # Load saved session (to avoid frequent logins and rate limits)
         L.load_session_from_file(settings.IG_LOGIN, filename=settings.SESSION_FILE)
         
         
 
         # Fetch the target user profile
         profile = instaloader.Profile.from_username(L.context, username)
+
+        # Try HD picture first, fallback to normal
+        try:
+            profile_pic_url = str(profile.profile_pic_url)
+        except AttributeError:
+            logging.warning(f"No profile picture found for {username}")
+            profile_pic_url = None
 
         
 
@@ -25,14 +35,14 @@ def scrape_instagram_profile(username):
             'posts': profile.mediacount,
             'is_verified': profile.is_verified,
             'external_url': profile.external_url,
-            'profile_pic_url': profile.profile_pic_url
-             }
+           "profile_pic_url": profile_pic_url,
+            }
     except instaloader.exceptions.ProfileNotExistsException:
-        print(f"Instagram profile '{username}' not found.")
+        logging.warning(f"Instagram profile '{username}' not found.")
         return None
     
     except Exception as e:
-        print(f"Error scraping Instagram: {e}")
+        logging.exception(f"Error scraping Instagram for {username}")
         return None
     
 
