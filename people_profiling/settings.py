94574a8cb86934905f9e59931e8ac4b014c50164
CarolGitonga <carolgitongaofficial@gmail.com>
2025-10-01 12:33:17 +0300

    Load images correctly

--- people_profiling/settings.py
+++ people_profiling/settings.py
@@ -143,8 +143,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (now on Cloudinary)
