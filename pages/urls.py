745b587b71a61fcbad7abdb96fbe6976484b3c62
CarolGitonga <carolgitongaofficial@gmail.com>
2025-10-23 12:46:50 +0300

    Create about page

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.landing_page, name="landing_page"),
+    path("about/", views.about_page, name="about"),
+
     
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def landing_page(request):
     # just render the landing page with empty results
     return render(request, "pages/landing.html", {"profile": []})
+
+def about_page(request):
+    return render(request, "pages/about.html")
