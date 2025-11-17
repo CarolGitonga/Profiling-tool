3c4471f6a8dd6258881af60b49a990a50fe7cf05
CarolGitonga <carolgitongaofficial@gmail.com>
2025-11-17 22:00:58 +0300

    Fix imports

--- profiles/utils/behavior.py
+++ profiles/utils/behavior.py
@@ -1,9 +1,10 @@
-from profiles.models import BehaviorAnalysis
+from profiles.models import BehavioralAnalysis
+
 
 def ensure_behavioral_record(profile):
     """
     Make sure a BehaviorAnalysis record exists for this profile.
     Called before behavioral analysis is queued.
     """
-    obj, _ = BehaviorAnalysis.objects.get_or_create(profile=profile)
+    obj, _ = BehavioralAnalysis.objects.get_or_create(profile=profile)
     return obj
